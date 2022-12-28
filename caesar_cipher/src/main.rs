74a13112a1b78b65a92aed7840140f22d020bd41
Abhishek <akanth1994@gmail.com>
2022-12-28 15:37:20 +0530

    Caesar cipher updated

--- caesar_cipher/src/main.rs
+++ caesar_cipher/src/main.rs
@@ -1,28 +1,26 @@
-use std::io;
+fn caesar_cipher(text: &str, shift: u8) -> String {
+    text.chars()
+        .map(|c| {
+            let base = if c.is_ascii_lowercase() {
+                'a' as u8
+            } else if c.is_ascii_uppercase() {
+                'A' as u8
+            } else {
+                return c;
+            };
+
+            let shifted = (c as u8 - base + shift) % 26 + base;
+            shifted as char
+        })
+        .collect()
+}
 
 fn main() {
- 
-   let key = 14;
+    let original = "Hello, World!";
+    let encrypted = caesar_cipher(original, 3);
+    let decrypted = caesar_cipher(&encrypted, 23);
 
-    println!("Please enter the text to be encrypted");
-    let mut plain_text = String::new();
-    io::stdin().read_line(&mut plain_text).expect("Failed to read line");
-
-    let mut chars:Vec<char> = plain_text.chars().collect();
-    chars.pop();
-    let mut encrypt = String::new();
-    for element in chars.iter() {
-        let char = element;
-        let base = if char.is_ascii_lowercase() {
-            'a' as u8
-        } else {
-            'A' as u8
-        };
-
-        let shifted = ((*char as u8 + key  - base) % 26 + base) as char;
-        encrypt.push(shifted);
-    }
-    
-    println!("Plaintext is {}", plain_text);
-    println!("Encrypted String is {}", encrypt);
+    println!("Original message: {}", original);
+    println!("Encrypted message: {}", encrypted);
+    println!("Decrypted message: {}", decrypted);
 }
